c95c7756d223ea37e095a1de1aa9b0a28846ed7f
Steve Clement <steve@localhost.lu>
2015-03-09 19:18:08 +0100

    - small fileName change

--- testImage.py
+++ testImage.py
@@ -12,14 +12,14 @@ matrix = Adafruit_RGBmatrix(32, 3)
 # The adafruit.png image has a couple columns of black pixels at
 # the right edge, so erasing after the scrolled image isn't necessary.
 matrix.Clear()
-image = Image.open("../images/100px-LightOn.svg.png")
+image = Image.open("../images/32px-LightOn.svg.png")
 image.load()
 for n in range(32, -image.size[0], -1):
 	matrix.SetImage(image.im.id, n, 1)
 	time.sleep(0.025)
 
 matrix.Clear()
-image = Image.open("../images/100px-LightOff.svg.png")
+image = Image.open("../images/32px-LightOff.svg.png")
 image.load()
 for n in range(32, -image.size[0], -1):
 	matrix.SetImage(image.im.id, n, 1)
